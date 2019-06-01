6a58b0f133100c937ab4c05b03a691c294ca8fad
Hyunsuk Lee <hs.lee.logan@gmail.com>
2019-06-01 22:41:56 +0900

    json 으로 Marshaling 시 timestamp 로 변환되는 JSONTime 추가

--- models/common.go
+++ models/common.go
@@ -1,11 +1,23 @@
 package models
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
+
+// JSONTime ...
+type JSONTime time.Time
 
 // CommonFields ...
 type CommonFields struct {
 	ID        uint       `gorm:"primary_key" json:"id"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at"`
+	CreatedAt JSONTime   `json:"created_at"`
+	UpdatedAt JSONTime   `json:"updated_at"`
+	DeletedAt *time.Time `json:"-"`
+}
+
+// MarshalJSON ...
+func (t JSONTime) MarshalJSON() ([]byte, error) {
+	stamp := time.Time(t).Unix()
+	return []byte(strconv.FormatInt(stamp, 10)), nil
 }
