77cd7632e11cce16cf38d7d5331241af44aab005
Logan <hs.lee.logan@gmail.com>
2019-09-09 14:21:17 +0900

    page, page_size 를 얻을때 정수 범위를 넘어가는 인자에 대한 처리 추가

--- handler/common.go
+++ handler/common.go
@@ -42,8 +42,10 @@ func createNewUser(user *models.User) (errPayload gin.H) {
 
 var (
 	errPageType      = errors.New("'page' must be integer")
+	errPageRange     = errors.New("'page' out of integer range")
 	errPageValue     = errors.New("'page' must not be less than zero")
 	errPageSizeType  = errors.New("'page_size' must be integer")
+	errPageSizeRange = errors.New("'page_size' out of integer range")
 	errPageSizeValue = errors.New("'page_size' must not be less than one")
 )
 
@@ -51,7 +53,16 @@ var (
 func Page(c *gin.Context) (int, error) {
 	page, err := strconv.Atoi(c.DefaultQuery("page", "0"))
 	if err != nil {
-		return 0, errPageType
+		e := err.(*strconv.NumError)
+		if e.Err == strconv.ErrSyntax {
+			return 0, errPageType
+
+		} else if e.Err == strconv.ErrRange {
+			return 0, errPageRange
+
+		}
+
+		return 0, err
 	}
 
 	if page < 0 {
@@ -65,7 +76,16 @@ func Page(c *gin.Context) (int, error) {
 func PageSize(c *gin.Context) (int, error) {
 	pageSize, err := strconv.Atoi(c.DefaultQuery("page_size", defaultPageSize))
 	if err != nil {
-		return 0, errPageSizeType
+		e := err.(*strconv.NumError)
+		if e.Err == strconv.ErrSyntax {
+			return 0, errPageSizeType
+
+		} else if e.Err == strconv.ErrRange {
+			return 0, errPageSizeRange
+
+		}
+
+		return 0, err
 	}
 
 	if pageSize < 1 {
