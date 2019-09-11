8d2637bf0d4e25a9b9ddb1ad4cbaafd901f7bf9a
Logan <hs.lee.logan@gmail.com>
2019-09-11 12:30:34 +0900

    깨지는 configs test 수정

--- configs/configs_test.go
+++ configs/configs_test.go
@@ -16,6 +16,6 @@ func TestDB(t *testing.T) {
 
 	// Assertions
 	conf := DB()
-	expected := "test_db_id:test_db_pw@/test_db_name?" + connOpt
+	expected := "test_db_id:test_db_pw@/test_db_name?" + dbConOpt
 	assert.Equal(t, conf.ConnectionString(), expected)
 }
