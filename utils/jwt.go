014f521181098a1d8661801f540a6c827d189ae6
Hyunsuk Lee <hs.lee.logan@gmail.com>
2019-10-03 00:04:16 +0900

    가입 이메일 확인 / 세션 토큰에 사용할 JWT 와 테스트 추가

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,33 +1,73 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/google/uuid"
 
 	"github.com/loganstone/auth/configs"
+	"github.com/loganstone/auth/models"
 )
 
 type Token struct {
 	jwt.Token
 }
 
-func NewJWTToken() *Token {
+func NewJWTToken(expireAfterSec int) *Token {
+	now := time.Now()
 	claims := jwt.MapClaims{
-		"aud": "audience",
-		"exp": time.Now().Add(time.Minute * time.Duration(5)).Unix(),
-		"jti": "reqeustID",
-		"iat": time.Now().Unix(),
-		"iss": "auth",
+		"aud": "",
+		"exp": now.Add(time.Second * time.Duration(expireAfterSec)).Unix(),
+		"jti": uuid.New().String(),
+		"iat": now.Unix(),
+		"iss": "auth", // TODO(hs.lee): 설정 가능하게 변경.
 		"nbf": 0,
-		"sub": "title",
+		"sub": "",
 	}
-	token := jwt.NewWithClaims(jwt.SigningMethodRS512, claims)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return &Token{
 		*token,
 	}
 }
 
-func (t *Token) Sign() (string, error) {
+func (t *Token) Signup(email string) (string, error) {
+	claims := t.Claims.(jwt.MapClaims)
+	claims["sub"] = "Signup"
+	claims["aud"] = email
 	return t.SignedString(configs.App().JWTSigninKey)
+}
+
+func (t *Token) Session(user *models.User) (string, error) {
+	data, err := json.Marshal(user)
+	if err != nil {
+		return "", err
+	}
+
+	claims := t.Claims.(jwt.MapClaims)
+	claims["sub"] = "Authorization"
+	claims["aud"] = user.Email
+	claims["user"] = string(data)
+	return t.SignedString(configs.App().JWTSigninKey)
+}
+
+func ParseJWTToken(signedString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(signedString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return configs.App().JWTSigninKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return nil, fmt.Errorf("invalid token")
+	}
 }
--- utils/jwt_test.go
+++ utils/jwt_test.go
@@ -7,9 +7,37 @@ import (
 	"gopkg.in/go-playground/assert.v1"
 )
 
+const (
+	testEmail = "test@email.com"
+)
+
 func TestNewJWTToken(t *testing.T) {
-	token := NewJWTToken()
+	token := NewJWTToken(1)
 	claims := token.Claims.(jwt.MapClaims)
 
 	assert.Equal(t, "auth", claims["iss"])
+	assert.Equal(t, "", claims["sub"])
+	assert.Equal(t, "", claims["aud"])
+}
+
+func TestParseToken(t *testing.T) {
+	token := NewJWTToken(5)
+	signupToken, err := token.Signup(testEmail)
+	assert.Equal(t, err, nil)
+
+	tokenData, err := ParseJWTToken(signupToken)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, testEmail, tokenData["aud"])
+	assert.Equal(t, "Signup", tokenData["sub"])
+}
+
+func TestParseTokenWithExpired(t *testing.T) {
+	token := NewJWTToken(-1)
+	signupToken, err := token.Signup(testEmail)
+	assert.Equal(t, err, nil)
+
+	_, err = ParseJWTToken(signupToken)
+	ve, ok := err.(*jwt.ValidationError)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, jwt.ValidationErrorExpired, ve.Errors)
 }
